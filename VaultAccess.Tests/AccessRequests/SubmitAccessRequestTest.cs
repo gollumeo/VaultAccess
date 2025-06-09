2e6eba076efb2f8d401b36e0a3ee7472e3ddf9ff
Golluméo <p.mauriello54@gmail.com>
2025-06-09 12:18:51 +0200

    test(access-requests): add unit tests for access request functionality
    Implemented tests to verify that an `AccessRequest` is created correctly, can be associated with a specific vault, and defaults to a "pending" status upon creation. These tests validate core behaviors of the `AccessRequest` entity.

--- VaultAccess.Tests/AccessRequests/SubmitAccessRequestTest.cs
+++ VaultAccess.Tests/AccessRequests/SubmitAccessRequestTest.cs
@@ -1,6 +1,32 @@
-﻿namespace VaultAccess.Tests.AccessRequests;
+﻿using FluentAssertions;
+using VaultAccess.Domain.Entities;
+
+namespace VaultAccess.Tests.AccessRequests;
 
 public class SubmitAccessRequestTest
 {
-    
+    [Fact]
+    public void AccessRequestIsCreatedUponUserRequest()
+    {
+        var accessRequest = new AccessRequest(Guid.Empty);
+
+        accessRequest.Should().BeOfType<AccessRequest>();
+    }
+
+    [Fact]
+    public void UserCanSubmitRequestToSpecificVault()
+    {
+        var vaultId = Guid.Empty;
+        var accessRequest = new AccessRequest(vaultId);
+
+        accessRequest.VaultId.Should().Be(vaultId);
+    }
+
+    [Fact]
+    public void AccessRequestMustBePendingUponCreation()
+    {
+        var accessRequest = new AccessRequest(Guid.Empty);
+        
+        accessRequest.Status.Should().Be("pending");
+    }
 }

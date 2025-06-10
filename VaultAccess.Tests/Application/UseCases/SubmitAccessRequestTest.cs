ffff17900b9109079d817436e6555df61e8de7fd
Golluméo <p.mauriello54@gmail.com>
2025-06-10 13:45:09 +0200

    test(application): add unit tests for SubmitAccessRequest use case
    Added comprehensive unit tests for the `SubmitAccessRequest` use case. These tests verify that the returned access request is linked to the correct user and vault, has the status set to "pending", and is of the

---
+++ VaultAccess.Tests/Application/UseCases/SubmitAccessRequestTest.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using VaultAccess.Application.Write;
+using VaultAccess.Domain.Entities;
+
+namespace VaultAccess.Tests.Application.UseCases;
+
+public class SubmitAccessRequestTest
+{
+    private readonly Guid _userId = Guid.NewGuid();
+    private readonly Guid _vaultId = Guid.NewGuid();
+    
+    [Fact]
+    public async Task ReturnsAccessRequest()
+    {
+        var useCase = new SubmitAccessRequest();
+        var result = await useCase.Execute(_userId, _vaultId);
+
+        result.Should().BeOfType<AccessRequest>();
+    }
+
+    [Fact]
+    public async Task ReturnedAccessRequestShouldBePending()
+    {
+        var useCase = new SubmitAccessRequest();
+        var result = await useCase.Execute(_userId, _vaultId);
+        
+        result.Status.Should().Be("pending");
+    }
+
+    [Fact]
+    public async Task ReturnedAccessRequestShouldBeLinkedToCorrectUser()
+    {
+        var useCase = new SubmitAccessRequest();
+        var result = await useCase.Execute(_userId, _vaultId);
+        
+        result.UserId.Should().Be(_userId);
+    }
+    
+    [Fact]
+    public async Task ReturnedAccessRequestShouldBeLinkedToCorrectVault()
+    {
+        var useCase = new SubmitAccessRequest();
+        var result = await useCase.Execute(_userId, _vaultId);
+        
+        result.VaultId.Should().Be(_vaultId);
+    }
+}

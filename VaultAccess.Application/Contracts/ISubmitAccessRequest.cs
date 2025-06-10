9ede4664b37596c7b30c5adcb14c0b5cc82ab51a
Golluméo <p.mauriello54@gmail.com>
2025-06-10 13:45:23 +0200

    feat(application): update ISubmitAccessRequest interface to include parameters
    Modified the `ISubmitAccessRequest.Execute` method to accept `userId` and `vaultId` as parameters. This change enables the interface to handle context-specific access request submissions, paving the way for more targeted implementations.

--- VaultAccess.Application/Contracts/ISubmitAccessRequest.cs
+++ VaultAccess.Application/Contracts/ISubmitAccessRequest.cs
@@ -4,5 +4,5 @@ namespace VaultAccess.Application.Contracts;
 
 public interface ISubmitAccessRequest
 {
-    public Task<AccessRequest> Execute();
+    public Task<AccessRequest> Execute(Guid userId, Guid vaultId);
 }

6dd7bad58ad822baf943c117afc0b6a2e9ab6f0a
Golluméo <p.mauriello54@gmail.com>
2025-06-10 14:48:16 +0200

    feat(application): add ApproveAccessRequest use case
    Introduced the `ApproveAccessRequest` use case along with its interface, `IApproveAccessRequest`, in the application layer. This sets up the structure for handling the approval of access requests, aligning with use case-driven design principles.

---
+++ VaultAccess.Application/Contracts/IApproveAccessRequest.cs
@@ -0,0 +1,8 @@
+﻿using VaultAccess.Domain.Entities;
+
+namespace VaultAccess.Application.Contracts;
+
+public interface IApproveAccessRequest
+{
+    public void Execute(AccessRequest accessRequest);
+}
---
+++ VaultAccess.Application/Write/ApproveAccessRequest.cs
@@ -0,0 +1,12 @@
+﻿using VaultAccess.Application.Contracts;
+using VaultAccess.Domain.Entities;
+
+namespace VaultAccess.Application.Write;
+
+public class ApproveAccessRequest : IApproveAccessRequest
+{
+    public void Execute(AccessRequest accessRequest)
+    {
+        throw new NotImplementedException();
+    }
+}

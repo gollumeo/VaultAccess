f9dbfb4348926b2c434a889adb2aa1926a05edf2
Golluméo <p.mauriello54@gmail.com>
2025-06-12 12:34:22 +0200

    feat(application): update IApproveAccessRequest to support async result
    Modified the `IApproveAccessRequest` interface to make the `Execute` method asynchronous, returning a `Task<Result<AccessRequest>>`. This allows better handling of asynchronous operations and aligns with modern async programming practices.

--- VaultAccess.Application/Contracts/IApproveAccessRequest.cs
+++ VaultAccess.Application/Contracts/IApproveAccessRequest.cs
@@ -1,8 +1,9 @@
-﻿using VaultAccess.Domain.Entities;
+﻿using Application;
+using VaultAccess.Domain.Entities;
 
 namespace VaultAccess.Application.Contracts;
 
 public interface IApproveAccessRequest
 {
-    public void Execute(AccessRequest accessRequest);
+    public Task<Result<AccessRequest>> Execute(AccessRequest accessRequest);
 }

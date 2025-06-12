0002ce90728f94bf611d9f4ca7fd8c39cdc8fca8
Golluméo <p.mauriello54@gmail.com>
2025-06-12 18:33:41 +0200

    refactor(application): return Result in SubmitAccessRequest Execute
    Replaced the `Task` return type with `Result` in the `SubmitAccessRequest.Execute` method. This change removes asynchronous execution and standardizes the use case response format, improving consistency and simplifying the application's flow.

--- VaultAccess.Application/Write/SubmitAccessRequest.cs
+++ VaultAccess.Application/Write/SubmitAccessRequest.cs
@@ -1,12 +1,13 @@
-﻿using VaultAccess.Application.Contracts;
+﻿using Application;
+using VaultAccess.Application.Contracts;
 using VaultAccess.Domain.Entities;
 
 namespace VaultAccess.Application.Write;
 
 public class SubmitAccessRequest : ISubmitAccessRequest
 {
-    public Task<AccessRequest> Execute(Guid userId, Guid vaultId)
+    public Result<AccessRequest> Execute(Guid userId, Guid vaultId)
     {
-        return Task.FromResult(new AccessRequest(vaultId, userId));
+        return Result<AccessRequest>.Success(new AccessRequest(vaultId, userId));
     }
 }

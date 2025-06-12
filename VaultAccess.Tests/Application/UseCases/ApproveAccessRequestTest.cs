43b695696c8091e9e22a053faa24607a175ca70c
Golluméo <p.mauriello54@gmail.com>
2025-06-12 18:34:03 +0200

    test(application): add test for duplicate approval in ApproveAccessRequest
    Introduced a new test to validate that an access request cannot be approved multiple times. Ensures that attempting to approve an already granted access request throws an `InvalidAccessRequest` exception, improving test coverage and error handling validation.

--- VaultAccess.Tests/Application/UseCases/ApproveAccessRequestTest.cs
+++ VaultAccess.Tests/Application/UseCases/ApproveAccessRequestTest.cs
@@ -2,6 +2,7 @@
 using VaultAccess.Application.Write;
 using VaultAccess.Domain.Entities;
 using VaultAccess.Domain.Enums;
+using VaultAccess.Domain.Exceptions;
 
 namespace VaultAccess.Tests.Application.UseCases;
 
@@ -11,14 +12,27 @@ public class ApproveAccessRequestTest
     private readonly Guid _vaultId = Guid.NewGuid();
 
     [Fact]
-    public async Task PendingAccessRequestShouldBeGranted()
+    public void PendingAccessRequestShouldBeGranted()
     {
         var accessRequest = new AccessRequest(_vaultId, _userId);
         var useCase = new ApproveAccessRequest();
 
-        var result = await useCase.Execute(accessRequest);
+        var result = useCase.Execute(accessRequest);
 
         result.IsSuccess.Should().BeTrue();
         accessRequest.Status.Should().Be(AccessRequestStatus.Granted);
+    }
+
+    [Fact]
+    public void CannotGrantAccessMultipleTimes()
+    {
+        var accessRequest = new AccessRequest(_vaultId, _userId);
+        var useCase = new ApproveAccessRequest();
+
+        useCase.Execute(accessRequest);
+        var approvalConstruction = () => useCase.Execute(accessRequest);
+
+        approvalConstruction.Should()
+            .ThrowExactly<InvalidAccessRequest>("Access request already granted.");
     }
 }

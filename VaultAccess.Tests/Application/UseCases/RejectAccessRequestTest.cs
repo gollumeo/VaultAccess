737a0373ed5ba8e0ae0791ccd853fc89e4eb91cb
Golluméo <p.mauriello54@gmail.com>
2025-06-13 02:56:04 +0200

    test(application): add unit tests for RejectAccessRequest use case
    Introduced unit tests for the `RejectAccessRequest` use case to validate its behavior. Tests cover rejecting pending requests, handling already rejected requests, and rejecting non-pending requests. Enhances test coverage and ensures correctness in rejection logic.

---
+++ VaultAccess.Tests/Application/UseCases/RejectAccessRequestTest.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using VaultAccess.Application.Write;
+using VaultAccess.Domain.Entities;
+using VaultAccess.Domain.Enums;
+using VaultAccess.Domain.Exceptions;
+
+namespace VaultAccess.Tests.Application.UseCases;
+
+public class RejectAccessRequestTest
+{
+    private readonly Guid _userId = Guid.NewGuid();
+    private readonly Guid _vaultId = Guid.NewGuid();
+
+    [Fact]
+    public void PendingAccessRequestCanBeRejected()
+    {
+        var accessRequest = new AccessRequest(_vaultId, _userId);
+        var useCase = new RejectAccessRequest();
+        var result = useCase.Execute(accessRequest);
+
+        result.IsSuccess.Should().BeTrue();
+        accessRequest.Status.Should().Be(AccessRequestStatus.Rejected);
+    }
+
+    [Fact]
+    public void CannotRejectAnAlreadyRejectedAccessRequest()
+    {
+        var accessRequest = new AccessRequest(_vaultId, _userId);
+        accessRequest.Reject();
+        var useCase = new RejectAccessRequest();
+        var useCaseExecution = () => useCase.Execute(accessRequest);
+
+        useCaseExecution.Should()
+            .ThrowExactly<InvalidAccessRequest>("Cannot reject an already rejected access request.");
+    }
+
+    [Fact]
+    public void CannotRejectNonPendingAccessRequest()
+    {
+        var accessRequest = new AccessRequest(_vaultId, _userId);
+        accessRequest.Approve();
+        var useCase = new RejectAccessRequest();
+        var useCaseExecution = () => useCase.Execute(accessRequest);
+
+        useCaseExecution.Should()
+            .ThrowExactly<InvalidAccessRequest>("Cannot reject an access request that is not pending.");
+    }
+}

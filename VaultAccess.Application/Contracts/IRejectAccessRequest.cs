21eabd0fc1828b824be5e313ad8825cc85aebca2
Golluméo <p.mauriello54@gmail.com>
2025-06-13 02:25:44 +0200

    feat(application): add IRejectAccessRequest interface
    Introduced the `IRejectAccessRequest` interface in the application layer. This interface defines the `Execute` method for rejecting access requests and returning a standardized `Result<AccessRequest>`. It lays the groundwork for implementing access request rejection functionality.

---
+++ VaultAccess.Application/Contracts/IRejectAccessRequest.cs
@@ -0,0 +1,9 @@
+﻿using Application;
+using VaultAccess.Domain.Entities;
+
+namespace VaultAccess.Application.Contracts;
+
+public interface IRejectAccessRequest
+{
+    public Result<AccessRequest> Execute(AccessRequest accessRequest);
+}

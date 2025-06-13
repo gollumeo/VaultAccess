a28b40d5dd86808754d1635e70faafe0ee4b79a0
Golluméo <p.mauriello54@gmail.com>
2025-06-13 02:42:10 +0200

    test(domain): replace RejectedAccessRequest with AccessRequestFakeFactory
    Replaced `RejectedAccessRequest` with `AccessRequestFakeFactory` to centralize the creation of access request objects in tests. This refactor improves consistency, allows more flexible test setups, and reduces redundancy in fake object handling.

---
+++ VaultAccess.Tests/Factory/AccessRequestFakeFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using VaultAccess.Domain.Entities;
+using VaultAccess.Domain.Enums;
+
+namespace VaultAccess.Tests.Factory;
+
+public static class AccessRequestFakeFactory
+{
+    public static AccessRequest WithStatus(Guid vaultId, Guid userId, AccessRequestStatus status)
+    {
+        var request = (AccessRequest)Activator.CreateInstance(
+            typeof(AccessRequest),
+            true
+        )!;
+
+        typeof(AccessRequest).GetField("<VaultId>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic)
+            ?.SetValue(request, vaultId);
+
+        typeof(AccessRequest).GetField("<UserId>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic)
+            ?.SetValue(request, userId);
+
+        typeof(AccessRequest).GetField("<Status>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic)
+            ?.SetValue(request, status);
+
+        return request;
+    }
+}

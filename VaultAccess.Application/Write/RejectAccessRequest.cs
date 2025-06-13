a40abbe7bba2adc7838ce287e5a821f64b9392c6
Golluméo <p.mauriello54@gmail.com>
2025-06-13 02:56:08 +0200

    feat(application): implement RejectAccessRequest use case
    Added the `RejectAccessRequest` class to handle rejection of access requests in the application layer. This implementation invokes the `Reject` method on an `AccessRequest` entity and returns the result. It completes the functionality for managing rejected access requests.

---
+++ VaultAccess.Application/Write/RejectAccessRequest.cs
@@ -0,0 +1,15 @@
+﻿using Application;
+using VaultAccess.Application.Contracts;
+using VaultAccess.Domain.Entities;
+
+namespace VaultAccess.Application.Write;
+
+public class RejectAccessRequest : IRejectAccessRequest
+{
+    public Result<AccessRequest> Execute(AccessRequest accessRequest)
+    {
+        accessRequest.Reject();
+
+        return Result<AccessRequest>.Success(accessRequest);
+    }
+}

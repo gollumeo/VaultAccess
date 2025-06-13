e304d9c891fef005442d54ddc5e0302e9e12442b
Golluméo <p.mauriello54@gmail.com>
2025-06-13 02:25:22 +0200

    test(domain): add fake RejectedAccessRequest for testing purposes
    Introduced a `RejectedAccessRequest` class in the test project to simulate access requests with a rejected status. This will aid in writing and organizing tests that require predefined access request scenarios.

---
+++ VaultAccess.Tests/Fakes/RejectedAccessRequest.cs
@@ -0,0 +1,11 @@
+﻿using VaultAccess.Domain.Entities;
+using VaultAccess.Domain.Enums;
+
+namespace VaultAccess.Tests.Fakes;
+
+public class RejectedAccessRequest(Guid vaultId, Guid userId) : AccessRequest(vaultId, userId)
+{
+    public new AccessRequestStatus Status { get; private set; } = AccessRequestStatus.Rejected;
+    public new Guid VaultId { get; } = vaultId;
+    public new Guid UserId { get; } = userId;
+}

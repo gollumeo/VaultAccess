02a48637c36ccfe564b7cea399e217465c59011e
Golluméo <p.mauriello54@gmail.com>
2025-06-14 17:12:51 +0200

    refactor(persistence): update AccessRequestEntityTypeConfiguration for model alignment
    Revised `AccessRequestEntityTypeConfiguration` to use `AccessRequestModel` instead of `AccessRequest`. Added unique index on composite key `VaultId` and `UserId`, and an explicit key on `Id`. This ensures proper mapping, uniqueness, and compatibility in the persistence layer.

--- VaultAccess.Persistence/EF/Configurations/AccessRequestEntityTypeConfiguration.cs
+++ VaultAccess.Persistence/EF/Configurations/AccessRequestEntityTypeConfiguration.cs
@@ -1,15 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using VaultAccess.Domain.Entities;
+using VaultAccess.Persistence.EF.Models;
 
 namespace VaultAccess.Persistence.EF.Configurations;
 
-public class AccessRequestEntityTypeConfiguration : IEntityTypeConfiguration<AccessRequest>
+public class AccessRequestEntityTypeConfiguration : IEntityTypeConfiguration<AccessRequestModel>
 {
-    public void Configure(EntityTypeBuilder<AccessRequest> builder)
+    public void Configure(EntityTypeBuilder<AccessRequestModel> builder)
     {
         builder.ToTable("AccessRequests");
+
         builder.HasKey(accessRequest => new { accessRequest.VaultId, accessRequest.UserId });
+        builder.HasKey(accessRequest => accessRequest.Id);
+
+        builder.HasIndex(accessRequest => new { accessRequest.VaultId, accessRequest.UserId }).IsUnique();
+
         builder.Property(accessRequest => accessRequest.Status).HasConversion<string>().IsRequired();
     }
 }

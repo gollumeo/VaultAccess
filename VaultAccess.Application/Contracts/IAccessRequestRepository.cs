293aa142b544c92add3b1e767c4fa276d5a586e2
Golluméo <p.mauriello54@gmail.com>
2025-06-14 15:58:34 +0200

    feat(application): add IAccessRequestRepository contract
    Introduced `IAccessRequestRepository` interface in the application layer. This defines methods for retrieving, saving, and listing access requests, setting the groundwork for repository pattern implementation and improving separation of concerns.

---
+++ VaultAccess.Application/Contracts/IAccessRequestRepository.cs
@@ -0,0 +1,10 @@
+﻿using VaultAccess.Domain.Entities;
+
+namespace VaultAccess.Application.Contracts;
+
+public interface IAccessRequestRepository
+{
+    AccessRequest? GetById(Guid id);
+    void Save(AccessRequest accessRequest);
+    IEnumerable<AccessRequest> ListForUser(Guid userId);
+}

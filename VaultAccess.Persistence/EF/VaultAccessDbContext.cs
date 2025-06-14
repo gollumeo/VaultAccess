4c4b42850c6ba8a9a3c91b3593c60937a140b26d
Golluméo <p.mauriello54@gmail.com>
2025-06-14 15:58:51 +0200

    feat(persistence): add VaultAccessDbContext for EF Core integration
    Introduced `VaultAccessDbContext` class in the persistence layer with EF Core integration. Configured it to use `AccessRequestEntityTypeConfiguration` for consistent entity mapping. This lays the foundation for interacting with the database for access request management.

---
+++ VaultAccess.Persistence/EF/VaultAccessDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VaultAccess.Domain.Entities;
+using VaultAccess.Persistence.EF.Configurations;
+
+namespace VaultAccess.Persistence.EF;
+
+public class VaultAccessDbContext(DbContextOptions<VaultAccessDbContext> options) : DbContext(options)
+{
+    private DbSet<AccessRequest> AccessRequests { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new AccessRequestEntityTypeConfiguration());
+    }
+}

691e7e30b92c118398805eee837380575d639ee3
Golluméo <p.mauriello54@gmail.com>
2025-06-14 15:58:44 +0200

    feat(persistence): add AccessRequestEntityTypeConfiguration
    Introduced EF Core configuration for the `AccessRequest` entity. It defines table mapping, composite keys for `VaultId` and `UserId`, and status property conversion. This ensures consistent mapping and database schema alignment for access request persistence.

---
+++ VaultAccess.Persistence/EF/Configurations/AccessRequestEntityTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VaultAccess.Domain.Entities;
+
+namespace VaultAccess.Persistence.EF.Configurations;
+
+public class AccessRequestEntityTypeConfiguration : IEntityTypeConfiguration<AccessRequest>
+{
+    public void Configure(EntityTypeBuilder<AccessRequest> builder)
+    {
+        builder.ToTable("AccessRequests");
+        builder.HasKey(accessRequest => new { accessRequest.VaultId, accessRequest.UserId });
+        builder.Property(accessRequest => accessRequest.Status).HasConversion<string>().IsRequired();
+    }
+}

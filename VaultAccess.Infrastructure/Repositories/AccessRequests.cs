224fd949384fc03cc00cee3096a4e246afd1ffeb
Golluméo <p.mauriello54@gmail.com>
2025-06-14 15:58:39 +0200

    feat(infrastructure): add AccessRequests repository implementation
    Added `AccessRequests` class implementing `IAccessRequestRepository` in the infrastructure layer. This serves as a placeholder repository for managing access requests, supporting operations like retrieval, saving, and listing by user. Code currently throws `NotImplementedException`, to be completed in subsequent commits.

---
+++ VaultAccess.Infrastructure/Repositories/AccessRequests.cs
@@ -0,0 +1,22 @@
+﻿using VaultAccess.Application.Contracts;
+using VaultAccess.Domain.Entities;
+
+namespace VaultAccess.Infrastructure.Repositories;
+
+public class AccessRequests :IAccessRequestRepository
+{
+    public AccessRequest? GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Save(AccessRequest accessRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<AccessRequest> ListForUser(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+}
